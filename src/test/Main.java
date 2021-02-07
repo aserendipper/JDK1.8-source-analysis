0b4df0009d7e21a01bfdd87ad7ade6add79ecf74
aserendipper <569482018@qq.com>
2021-02-07 17:44:58 +0800

    add JDK1.8 source

---
+++ src/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Map<String, Integer> map = new HashMap<>();
+        map.put("one", 1);
+        map.put("two", 2);
+    }
+}
