a4b346a9ac6cf1d3151177377a06ac3ec855e6dc
aserendipper <569482018@qq.com>
2021-02-23 19:00:08 +0800

    💡java.lang.Void

--- src/jdk8/java/lang/Void.java
+++ src/jdk8/java/lang/Void.java
@@ -34,17 +34,17 @@ package java.lang;
  * @since   JDK1.1
  */
 public final
-class Void {
+class Void {  //Void类是一个不可实例化的占位符类，用于表示对Java关键字void的类对象的引用。也可用在泛型中，如需要一个返回值为void的泛型。还可用在形参中，仅仅表示占位。
 
     /**
      * The {@code Class} object representing the pseudo-type corresponding to
      * the keyword {@code void}.
      */
     @SuppressWarnings("unchecked")
-    public static final Class<Void> TYPE = (Class<Void>) Class.getPrimitiveClass("void");
+    public static final Class<Void> TYPE = (Class<Void>) Class.getPrimitiveClass("void");  //Void的原始类型  可以用于判断某个方法的返回值是否为void：if (getClass().getMethod("foo").getReturnType() == Void.TYPE)
 
     /*
      * The Void class cannot be instantiated.
      */
-    private Void() {}
+    private Void() {}  //无法被实例化
 }
