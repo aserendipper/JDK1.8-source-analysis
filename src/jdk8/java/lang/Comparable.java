51862f6d6a825d9d8dcd0cd120f3ac77df250657
aserendipper <569482018@qq.com>
2021-02-26 18:14:49 +0800

    💡java.lang.Comparable

--- src/jdk8/java/lang/Comparable.java
+++ src/jdk8/java/lang/Comparable.java
@@ -93,6 +93,18 @@ import java.util.*;
  * @see java.util.Comparator
  * @since 1.2
  */
+
+/**
+ * 内部比较器，常用作自然排序接口，需要实现内部的compareTo方法
+ *
+ * 内部比较器的特点是：嵌入式
+ * 其比较行为必须在待比较对象内部实现
+ *
+ * 一个类如果实现了Comparable接口，就意味着“该类本身支持排序”，并且可以直接通过Arrays.sort()或Collections.sort()进行排序
+ * 当然，一个类如果没有实现Comparable接口，也可以挂载外部比较器Comparator进行排序
+ *
+ * 注：区别于外部比较器Comparator
+ */
 public interface Comparable<T> {
     /**
      * Compares this object with the specified object for order.  Returns a
@@ -132,6 +144,12 @@ public interface Comparable<T> {
      * @throws NullPointerException if the specified object is null
      * @throws ClassCastException if the specified object's type prevents it
      *         from being compared to this object.
+     */
+    /**
+     * 通过 x.compareTo(y) 来“比较x和y的大小”：
+     * 返回“负数”，意味着“x<y”
+     * 返回“零”，意味着“x==y”
+     * 返回“正数”，意味着“x>y”
      */
     public int compareTo(T o);
 }
