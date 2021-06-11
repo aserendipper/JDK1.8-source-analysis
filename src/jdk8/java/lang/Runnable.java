224484f97cd01a8909e8e8287db0365e8547d13e
aserendipper <569482018@qq.com>
2021-06-11 18:53:42 +0800

    💡java.lang.Runnable

--- src/jdk8/java/lang/Runnable.java
+++ src/jdk8/java/lang/Runnable.java
@@ -51,6 +51,21 @@ package java.lang;
  * @see     java.lang.Thread
  * @see     java.util.concurrent.Callable
  * @since   JDK1.0
+ *
+ * Runnable表示一类无返回值，且不抛出异常的任务
+ *
+ * Runnable类任务通常由Thread直接执行，也可以交给【任务执行器】Executor去执行
+ * 此外，Runnable还可以经过适配器的装配，与Callable类型、Future类型等配合使用
+ *
+ * 该接口已函数化：
+ * Runnable runnable = new Runnable() {
+ *     @Override
+ *     public void run() {
+ *         System.out.println("Runnable");
+ *     }
+ * };
+ * 可以简写为：
+ * Runnable runnable = () -> { System.out.println("Runnable"); };
  */
 @FunctionalInterface
 public interface Runnable {
@@ -64,6 +79,8 @@ public interface Runnable {
      * take any action whatsoever.
      *
      * @see     java.lang.Thread#run()
+     *
+     * 任务执行入口
      */
     public abstract void run();
 }
